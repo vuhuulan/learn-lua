731a72bf7a0b227f03512cb43c73c6dd8178586f
vuhuulan <vuhuulan18@gmail.com>
2025-03-18 01:01:36 +0900

    feat: add tictactoe game

---
+++ src/tictactoe.lua
@@ -0,0 +1,42 @@
+local boxes = {}
+do
+    for _ = 1, 9 do
+        table.insert(boxes, " ")
+    end
+end
+
+---@return number
+local function get_move()
+    io.write("Your move: row (1-3): ")
+    local row = io.read()
+
+    io.write("Your move: col (1-3): ")
+    local col = io.read()
+
+    local pos_idx = col + 3 * (row - 1)
+    return pos_idx
+end
+
+---@param b string[] Boxes containing state of the game
+---@return string
+local function get_pretty_board(b)
+    local pretty_board = ""
+    local col_seperator = " - + - + - "
+    for r = 1, 3 do
+        local col = (" " .. b[3 * (r - 1) + 1] .. " | " .. b[3 * (r - 1) + 2] .. " | " .. b[3 * (r - 1) + 3])
+        pretty_board = pretty_board .. col
+        if r ~= 3 then
+            pretty_board = pretty_board .. "\n" .. col_seperator .. "\n"
+        end
+    end
+    return pretty_board
+end
+
+---@return nil
+local function main()
+    local pos = get_move()
+    print(pos .. " " .. boxes[pos])
+    print(get_pretty_board(boxes))
+end
+
+main()
